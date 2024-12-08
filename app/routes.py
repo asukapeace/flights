8c7deb7dbc7f6931540901eb0fbcc5b439f10832
asukapeace <asukapeace@hotmail.co.uk>
2024-12-08 12:38:46 +0000

    add departures end point

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, request
 from app import app 
 from opensky_api import OpenSkyApi
+import requests
+from datetime import datetime, timedelta
 
 def _getAPI():
     return OpenSkyApi()
@@ -9,3 +11,16 @@ def _getAPI():
 def front_page():
     #TODO put more here
     return render_template('index.html', data={'Message' : 'Welcome to my page about flights'})
+
+@app.route('/departures', methods=['GET'])
+def arrivals():
+    # for now use heathrow as default
+    # TODO find out how to map to the ICAO code
+
+    now = datetime.now()
+    airport = request.args.get('icao', 'EGLL')
+    start_time = int(request.args.get('begin', now.timestamp()))
+    end_time = int(request.args.get('end', (now + timedelta(minutes = 10)).timestamp()))
+
+    departures = _getAPI().get_departures_by_airport(airport, start_time, end_time)
+    return render_template('flight_info.html', data=departures)
