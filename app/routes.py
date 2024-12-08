0ff58579437c902889d5c870c3bc9e1276f98ef8
asukapeace <asukapeace@hotmail.co.uk>
2024-12-08 18:13:38 +0000

    make departure page format as table

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from app import app
 from opensky_api import OpenSkyApi
 import requests
 from datetime import datetime, timedelta
-
+import json
 def _getAPI():
     return OpenSkyApi()
 
@@ -23,4 +23,16 @@ def departures():
     end_time = int(request.args.get('end', (now + timedelta(minutes = 30)).timestamp()))
 
     departures = _getAPI().get_departures_by_airport(airport, start_time, end_time)
-    return render_template('flight_info.html', data=departures)
+    headers = departures[0].__dict__.keys()
+
+    data=[]
+    for d in departures:
+        flightInfo = {}
+        flightInfo['ICAO'] =  d.icao24
+        flightInfo['Call sign'] = d.callsign
+        flightInfo['Estimated horiz distance from dep airport'] = d.estDepartureAirportHorizDistance
+        flightInfo['Estimated vert distance from dep airport'] = d.estDepartureAirportVertDistance
+        flightInfo['Estimated arrival airport'] = d.estArrivalAirport
+        flightInfo['Time last seen'] = datetime.fromtimestamp(d.lastSeen).strftime('%Y-%m-%d %H:%M:%S') if d.lastSeen else d.lastSeen
+        data.append(flightInfo)
+    return render_template('flight_info.html', data=data)
