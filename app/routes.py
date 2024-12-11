20eaae0204faf73d17133f0531a94bf1af627291
asukapeace <asukapeace@hotmail.co.uk>
2024-12-11 19:41:58 +0000

    add arrivals end point

--- app/routes.py
+++ app/routes.py
@@ -13,18 +13,29 @@ def front_page():
 
 @app.route('/departureQuery', methods=['GET', 'POST'])
 def departureQuery():
-    return render_template('query.html')
+    return render_template('query.html', data={'departures' : True})
 
-@app.route('/departures', methods=['GET'])
-def departures():
-    # for now use heathrow as default
+@app.route('/arrivalsQuery', methods=['GET', 'POST'])
+def arrivalsQuery():
+    return render_template('query.html', data={'arrivals' : True})
+
+def _queryAndProcess(requestArgs, func):
     # TODO find out how to map to the ICAO code
+/*************  ✨ Codeium Command ⭐  *************/
+    """
+    Handles the POST requests from the query forms.
 
-    airport = request.args.get('icao')
-    start_time = int(datetime.strptime(request.args.get('begin'), "%Y-%m-%dT%H:%M").timestamp())
-    end_time = int(datetime.strptime(request.args.get('end'), "%Y-%m-%dT%H:%M").timestamp())
+    requestArgs: the GET request arguments
+    func: the function to call to get the data
 
-    departures = _getAPI().get_departures_by_airport(airport, start_time, end_time)
+    :return: a rendered template with the data
+    """
+/******  62c5b124-e09e-410f-bdb2-38d72fb14325  *******/
+    airport = requestArgs.get('icao')
+    start_time = int(datetime.strptime(requestArgs.get('begin'), "%Y-%m-%dT%H:%M").timestamp())
+    end_time = int(datetime.strptime(requestArgs.get('end'), "%Y-%m-%dT%H:%M").timestamp())
+
+    departures = func(airport, start_time, end_time)
     data=[]
     if departures:
         headers = departures[0].__dict__.keys()
@@ -39,3 +50,11 @@ def departures():
             flightInfo['Time last seen'] = datetime.fromtimestamp(d.lastSeen).strftime('%Y-%m-%d %H:%M:%S') if d.lastSeen else d.lastSeen
             data.append(flightInfo)
     return render_template('flight_info.html', data=data)
+
+@app.route('/arrivals', methods=['GET'])
+def arrivals():
+    return _queryAndProcess(request.args, _getAPI().get_arrivals_by_airport)
+
+@app.route('/departures', methods=['GET'])
+def departures():
+    return _queryAndProcess(request.args, _getAPI().get_departures_by_airport)
